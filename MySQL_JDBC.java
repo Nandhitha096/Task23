52dc57dcd995922e111a35fdd9e1087d03534d7b
Nandhitha096 <nandhitha096@gmail.com>
2024-08-27 22:57:02 +0530

    JDBC program commit

--- MySQL_JDBC.java
+++ MySQL_JDBC.java
@@ -9,23 +9,25 @@ public class MySQL_JDBC {
 
 	public static void main(String[] args) throws Exception {
 		
-				//creating connection
+				//creating connection to SQL workbench
 		
-				Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/task_23", "root" , "Viji@9843116611");
+				Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/task_23", "root" , "abc@12345");
 				
 				//creating statement
 				
 				Statement s = con.createStatement();
 				
-				//String ins1 = "insert into employees (values (101,'Jenny',25,10000)";
-				String ins2= "insert into employees values (102,'Jacky',30,20000)";
+				//inserting values into employees table
+				
+				String ins1 = "insert into employees (values (101,'Jenny',25,10000)";
+				String ins2=  "insert into employees values (102,'Jacky',30,20000)";
 				String ins3 = "insert into employees values (103,'Joe',20,40000)";
 				String ins4 = "insert into employees values (104,'John',40,80000)";
 				String ins5 = "insert into employees values (105,'Shameer',25,90000)";
 				
-				//executing statements
+				//executing statement 
 				
-				//s.execute(ins1);
+				s.execute(ins1);
 				s.execute(ins2);
 				s.execute(ins3);
 				s.execute(ins4);
